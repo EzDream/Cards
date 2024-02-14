977c5352bf0ab3582a97aa42e2089fc8a1dd7b5c
P <ezdream@icloud.com>
2024-02-14 15:23:33 +1300

    feats: add CardRow

--- Cards/Presentation/CardList/Components/CardRow.swift
+++ Cards/Presentation/CardList/Components/CardRow.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct CardRow: View {
+    var card: CardModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Text(card.creditCardNumber ?? "")
+                .bold()
+                .font(.system(.title, design: .rounded))
+                .lineLimit(1)
+                .minimumScaleFactor(0.7)
+        }
+        .frame(maxWidth: .infinity)
+        .frame(maxHeight: 180)
+        .overlay(alignment: .bottomLeading, content: {
+            HStack(spacing: 5) {
+                Text("Expires:")
+                    .font(.footnote)
+                    .foregroundColor(.primary.opacity(0.5))
+                Text(card.creditCardExpiryDate ?? "")
+                    .font(.footnote)
+                    .bold()
+            }
+        })
+        .overlay(alignment: .top, content: {
+            HStack {
+                Spacer()
+                Text(card.creditCardType ?? "")
+                    .bold()
+                    .font(.system(.largeTitle, design: .serif))
+            }
+        })
+        .padding(25)
+        .background(.background)
+        .cornerRadius(20)
+        .padding()
+        .shadow(color: .primary.opacity(0.5), radius: 10, x: 0, y: 5)
     }
 }
 
 #Preview {
-    CardRow()
+    CardRow(card: MockCards.card)
 }
