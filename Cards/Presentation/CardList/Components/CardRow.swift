d0d45c7e6ede1a196b878629ad3fd040272afc3c
P <ezdream@icloud.com>
2024-02-14 17:40:08 +1300

    fixed: bookmark click areas

--- Cards/Presentation/CardList/Components/CardRow.swift
+++ Cards/Presentation/CardList/Components/CardRow.swift
@@ -14,13 +14,38 @@ struct CardRow: View {
         return card.isFavorite == true ? "heart.fill" : "heart"
     }
 
+    func cardTypeView() -> some View {
+        VStack {
+            HStack {
+                Button {
+                    withAnimation {
+                        card.isFavorite.toggle()
+                    }
+                } label: {
+                    Image(systemName: bookmark)
+                        .font(.system(size: 23))
+                        .foregroundColor(card.isFavorite ? .red : .primary.opacity(0.5))
+                }
+                .buttonStyle(.plain)
+                Spacer()
+                Text(card.model.cardType.capitalized)
+                    .bold()
+                    .font(.system(.headline, design: .monospaced))
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
+            }
+            Spacer()
+        }
+    }
+
     var body: some View {
-        ZStack {
+        ZStack(alignment: .center) {
             Text(card.model.cardNumber)
                 .bold()
                 .font(.system(.title, design: .rounded))
                 .lineLimit(1)
                 .minimumScaleFactor(0.7)
+            cardTypeView()
         }
         .frame(maxWidth: .infinity)
         .frame(height: 180)
@@ -32,24 +57,6 @@ struct CardRow: View {
                 Text(card.model.cardExpiryDate)
                     .font(.footnote)
                     .bold()
-            }
-        })
-        .overlay(alignment: .top, content: {
-            HStack {
-                Button {
-                    card.isFavorite.toggle()
-                } label: {
-                    Image(systemName: bookmark)
-                        .font(.system(size: 20))
-                }
-                .background(.red)
-
-                Spacer()
-                Text(card.model.cardType.capitalized)
-                    .bold()
-                    .font(.system(.headline, design: .monospaced))
-                    .lineLimit(1)
-                    .minimumScaleFactor(0.5)
             }
         })
         .padding(25)
